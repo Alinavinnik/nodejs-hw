7eea35ca16256f3946cad0aedbb33fb7deb15562
Alina <alinavinnikhgfff@gmail.com>
2026-07-20 14:59:32 +0300

    add authenticate

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,31 @@
+import createHttpError from 'http-errors';
+import { Session } from '../models/session';
+import { User } from '../models/user';
+
+export async function authenticate(req, res, next) {
+  const { sessionId, accessToken } = req.cookies;
+  if (!accessToken) {
+    throw createHttpError(401, 'Missing access token');
+  }
+
+  const session = await Session.findOne({
+    _id: sessionId,
+    accessToken,
+  });
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const isAccessTokenExpired = session.accessTokenValidUntil < new Date();
+  if (isAccessTokenExpired) {
+    throw createHttpError(401, 'Access token expired');
+  }
+
+  const user = await User.findOne(session.userId);
+  if (!user) {
+    throw createHttpError(401);
+  }
+  req.user = user;
+
+  next();
+}
